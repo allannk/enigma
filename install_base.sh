17661b88812a74de32b73711de88908a21de53cb
Allan Nørgaard Kristensen <allannk@gmail.com>
2017-03-06 14:45:58 +0100

    Needed flag and fixes

--- install_base.sh
+++ install_base.sh
@@ -5,12 +5,12 @@ sudo chown $USER /srv -R
 sudo chmod a+rwx /tmp 
 
 # Create projects folder and mount folders
-mkdir /srv/projects
-sudo mkdir /mnt/hdd 
-sudo mkdir /mnt/usb
+mkdir -p /srv/projects
+sudo mkdir -p /mnt/hdd 
+sudo mkdir -p /mnt/usb
 
 # Install basic packages
-sudo pacman --noconfirm -S cmake gcc5 bash-completion eigen cuda yasm ladspa hardening-wrapper libfdk-aac nvidia-utils opencl-nvidia libglvnd boost glfw-x11 glm enca libcaca python python-numpy gimp evince vlc networkmanager qt4
+sudo pacman --noconfirm --needed -S cmake gcc5 bash-completion eigen cuda yasm ladspa hardening-wrapper libfdk-aac nvidia-utils opencl-nvidia libglvnd boost glfw-x11 glm enca libcaca python python-numpy gimp evince vlc networkmanager qt4
 
 # Install yaourt if not present
 # TODO
@@ -20,8 +20,11 @@ sudo pacman --noconfirm -U ffmpeg-git-3.3.r83754.gef86488696-1-x86_64.pkg.tar.xz
 # Alternative: pacman -S ffmpeg-git
 
 # Install bazel and post-remove protobuf (makedepend)
-yaourt --m-arg "--skippgpcheck" --noconfirm -S bazel
+yaourt --m-arg "--skippgpcheck" --noconfirm --needed -S bazel
 sudo pacman --noconfirm -R protobuf
+
+# Install cuDNN
+yaourt -S cudnn
 
 # Compile & Install OpenCV
 #install_opencv
