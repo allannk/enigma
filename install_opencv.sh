4db98c429eaff8d3729ea7c796b927a3a43a0c15
Allan Nørgaard Kristensen <allannk@gmail.com>
2017-03-06 13:42:01 +0100

    Added install script for opencv

--- install_opencv.sh
+++ install_opencv.sh
@@ -3,10 +3,9 @@
 git clone https://github.com/opencv/opencv.git /usr/src
 git clone https://github.com/opencv/opencv_contrib.git /usr/src
 
-
 mkdir /usr/src/opencv/build
 cd /usr/src/opencv/build
 
-CXX=/usr/bin/g++-5 GCC=/usr/bin/gcc-5 CC=/usr/bin/gcc-5 cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_PNG=OFF -DWITH_FFMPEG=OFF -DWITH_CUDA=OFF -DWITH_IPP=OFF -DENABLE_PRECOMPILED_HEADERS=OFF -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
+CXX=/usr/bin/g++-5 GCC=/usr/bin/gcc-5 CC=/usr/bin/gcc-5 sudo cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_PNG=OFF -DWITH_FFMPEG=OFF -DWITH_CUDA=OFF -DWITH_IPP=OFF -DENABLE_PRECOMPILED_HEADERS=OFF -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
 make -j$(nproc)
-sudo make install
+make install
