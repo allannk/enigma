332231447c3eff3e937ac4543c24787433472f27
Allan Nørgaard Kristensen <allannk@gmail.com>
2017-03-06 13:36:44 +0100

    Fixed permissions for /srv and added mkdir

--- install_pipeline.sh
+++ install_pipeline.sh
@@ -1,4 +1,9 @@
 #!/bin/zsh
 
+sudo chown $USER /srv -r
+sudo chgrp $USER /srv -r
+
+mkdir /srv/projects
+
 git clone https://github.com/recoord/egon.git /srv/projects
 ln -s /srv/projects/egon $HOME/egon
