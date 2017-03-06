e3234da63f9ad537eaf0b916c1aa49ef7549430b
Allan Nørgaard Kristensen <allannk@gmail.com>
2017-03-06 13:10:13 +0100

    Added more install files

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
-yaourt -S ffmpeg-git
+#pacman -S yasm ladspa hardening-wrapper libfdk-aac 
+#yaourt -S ffmpeg-git
+pacman -U ffmpeg-git-3.3.r83754.gef86488696-1-x86_64.pkg.tar.xz
---
+++ mount_hdd.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+# Mount HDD under /mnt/hdd
+# Create symlink from /home/<user>/veo to /mnt/hdd
