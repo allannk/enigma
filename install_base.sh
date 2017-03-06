21f0a2adc68c9485d207d78959b47fa147f63b08
Allan Nørgaard Kristensen <allannk@gmail.com>
2017-03-06 14:35:34 +0100

    Updated with basic install script and fixed bug in opencv

---
+++ install_base.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+# Own folders we will be using!
+sudo chown $USER /srv -R
+sudo chgrp $USER /srv -R
+sudo chmod a+rwx /tmp 
+
+# Create projects folder and mount folders
+mkdir /srv/projects
+sudo mkdir /mnt/hdd 
+sudo mkdir /mnt/usb
+
+# Install basic packages
+pacman --noconfirm -S cmake gcc5 bash-completion eigen cuda yasm ladspa hardening-wrapper libfdk-aac nvidia-utils opencl-nvidia libglvnd boost glfw-x11 glm enca libcaca python python-numpy gimp evince vlc networkmanager qt4
+
+# Install yaourt if not present
+# TODO
+
+# Install ffmpeg-git
+pacman --noconfirm -U ffmpeg-git-3.3.r83754.gef86488696-1-x86_64.pkg.tar.xz
+# Alternative: pacman -S ffmpeg-git
+
+# Install bazel and post-remove protobuf (makedepend)
+yaourt --m-arg "--skippgpcheck" --noconfirm -S bazel
+pacman --noconfirm -R protobuf
+
+# Compile & Install OpenCV
+#install_opencv
+
+# Compile and Install TensorFlow
+#install_tensorflow
--- install_pipeline.sh
+++ install_pipeline.sh
@@ -1,9 +1,6 @@
 #!/bin/zsh
 
-sudo chown $USER /srv -R
-sudo chgrp $USER /srv -R
 
-mkdir /srv/projects
 
 git clone https://github.com/recoord/egon.git /srv/projects/egon
 ln -s /srv/projects/egon $HOME/egon
