8b498543817fe1280092cd21d7c5d7daa9983389
Allan Nørgaard Kristensen <allannk@gmail.com>
2017-03-06 13:49:05 +0100

    fixed git clone in pipeline install

--- install_pipeline.sh
+++ install_pipeline.sh
@@ -5,5 +5,5 @@ sudo chgrp $USER /srv -R
 
 mkdir /srv/projects
 
-git clone https://github.com/recoord/egon.git /srv/projects
+git clone https://github.com/recoord/egon.git /srv/projects/egon
 ln -s /srv/projects/egon $HOME/egon
