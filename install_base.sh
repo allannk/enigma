c68fc77db51fb243ed567c6350dba378b6f87c4d
Allan Nørgaard Kristensen <allannk@gmail.com>
2017-03-07 11:01:09 +0100

    Added NFS

--- install_base.sh
+++ install_base.sh
@@ -18,11 +18,23 @@ sudo chmod a+rwx /tmp
 
 # Create projects folder and mount folders
 mkdir -p /srv/projects
-sudo mkdir -p /mnt/hdd 
+sudo mkdir -p /mnt/hdd
 sudo mkdir -p /mnt/usb
 
 # Mount HDD and NFS drive
-# TODO
+sudo mkdir -p /mnt/data
+sudo mkdir -p /mnt/video
+if grep -e sportcaster /etc/fstab >/dev/null; then
+	echo -e "Dendron Mount Point already specified in /etc/fstab. If broken, remove these lines first!\n"
+	exit 1
+else
+	echo -e "Adding dendron NFS to '/etc/fstab' for automounting on boot ...\n"
+	echo -e "# Mount Dendron NFS" | sudo tee -a /etc/fstab >/dev/null
+	echo -e "192.168.0.6:/mnt/video /mnt/video nfs rw,rsize=8192,wsize=8192,timeo=14,_netdev,noauto" | sudo tee -a /etc/fstab >/dev/null
+	echo -e "192.168.0.6:/mnt/data /mnt/data nfs rw,rsize=8192,wsize=8192,timeo=14,_netdev,noauto" | sudo tee -a /etc/fstab >/dev/null	
+fi
+echo -e "Remounting all drives in /etc/fstab ..."
+sudo mount -a
 
 
 # ----------------------------
