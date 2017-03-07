89edaf1895f209d63bc9e5b1cd1aff9845d97cd7
Allan Nørgaard Kristensen <allannk@gmail.com>
2017-03-07 11:07:57 +0100

    fixed linebreak

--- install_base.sh
+++ install_base.sh
@@ -4,9 +4,7 @@
 # Install/Remove Base Packages
 # ----------------------------
 sudo pacman --noconfirm --needed -S cmake gcc5 bash-completion eigen cuda yasm ladspa hardening-wrapper libfdk-aac nvidia-utils opencl-nvidia libglvnd boost glfw-x11 glm enca libcaca python python-numpy gimp evince vlc networkmanager qt4 netbeans atom
-sudo pacman --noconfirm -R qt5-webengine akonadi-contacts akonadi-calendar calendarsupport akonadiconsole akonadi-calendar-tools eventviews incidenceeditor korganizer libkdepim kdepim-addons kdepim-runtime kgpg knotes kmail kalarm kaddressbook mailimporter pimcommon nfs-utils
-pim-data-exporter kdepim-apps-libs mailcommon pim-storage-service-manager mbox-importer messagelib libgravatar libksieve kontact pim-sieve-editor blogilo akonadi-import-wizard kmail-account-wizard grantlee-editor akregator
-
+sudo pacman --noconfirm -R qt5-webengine akonadi-contacts akonadi-calendar calendarsupport akonadiconsole akonadi-calendar-tools eventviews incidenceeditor korganizer libkdepim kdepim-addons kdepim-runtime kgpg knotes kmail kalarm kaddressbook mailimporter pimcommon nfs-utils pim-data-exporter kdepim-apps-libs mailcommon pim-storage-service-manager mbox-importer messagelib libgravatar libksieve kontact pim-sieve-editor blogilo akonadi-import-wizard kmail-account-wizard grantlee-editor akregator
 
 # ----------------------------
 #     Manipulate Folders
@@ -30,8 +28,8 @@ if grep -e sportcaster /etc/fstab >/dev/null; then
 else
 	echo -e "Adding dendron NFS to '/etc/fstab' for automounting on boot ...\n"
 	echo -e "# Mount Dendron NFS" | sudo tee -a /etc/fstab >/dev/null
-	echo -e "192.168.0.6:/mnt/video /mnt/video nfs rw,rsize=8192,wsize=8192,timeo=14,_netdev,noauto" | sudo tee -a /etc/fstab >/dev/null
-	echo -e "192.168.0.6:/mnt/data /mnt/data nfs rw,rsize=8192,wsize=8192,timeo=14,_netdev,noauto" | sudo tee -a /etc/fstab >/dev/null	
+	echo -e "192.168.0.6:/mnt/video /mnt/video nfs rw,rsize=8192,wsize=8192,timeo=14,_netdev,auto" | sudo tee -a /etc/fstab >/dev/null
+	echo -e "192.168.0.6:/mnt/data /mnt/data nfs rw,rsize=8192,wsize=8192,timeo=14,_netdev,auto" | sudo tee -a /etc/fstab >/dev/null	
 fi
 echo -e "Remounting all drives in /etc/fstab ..."
 sudo mount -a
