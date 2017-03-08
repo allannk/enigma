5604aac1e9152c9cf72aae8d4714443f181d9d42
Allan Nørgaard Kristensen <allannk@gmail.com>
2017-03-08 08:20:15 +0100

    Added bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Basic System Packages
+sudo pacman --noconfirm --needed -S cmake wget bash-completion python python2 python-numpy python2-numpy nfs-utils
+
+# Install GFX Drivers
+sudo pacman --noconfirm --needed -S https://archive.archlinux.org/packages/n/nvidia/nvidia-375.26-6-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/n/nvidia-utils/nvidia-utils-375.26-4-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/n/nvidia-libgl/nvidia-libgl-375.26-3-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/o/opencl-nvidia/opencl-nvidia-375.26-4-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/l/libxnvctrl/libxnvctrl-375.26-1-x86_64.pkg.tar.xz
