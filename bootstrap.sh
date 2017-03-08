fc8acb64a760952968ca5e723462385f221adef8
Allan Nørgaard Kristensen <allannk@gmail.com>
2017-03-08 08:26:48 +0100

    Fixed -S -> -U

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@
 sudo pacman --noconfirm --needed -S cmake wget bash-completion python python2 python-numpy python2-numpy nfs-utils
 
 # Install GFX Drivers
-sudo pacman --noconfirm --needed -S https://archive.archlinux.org/packages/n/nvidia/nvidia-375.26-6-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/n/nvidia-utils/nvidia-utils-375.26-4-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/n/nvidia-libgl/nvidia-libgl-375.26-3-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/o/opencl-nvidia/opencl-nvidia-375.26-4-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/l/libxnvctrl/libxnvctrl-375.26-1-x86_64.pkg.tar.xz
+sudo pacman --noconfirm --needed -U https://archive.archlinux.org/packages/n/nvidia/nvidia-375.26-6-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/n/nvidia-utils/nvidia-utils-375.26-4-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/n/nvidia-libgl/nvidia-libgl-375.26-3-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/o/opencl-nvidia/opencl-nvidia-375.26-4-x86_64.pkg.tar.xz https://archive.archlinux.org/packages/l/libxnvctrl/libxnvctrl-375.26-1-x86_64.pkg.tar.xz
